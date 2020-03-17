8a37e09ee3e4f35845c58788e14dc32364b8b8fb
anupo <anupomr@yahoo.com>
2020-03-17 10:17:34 -0400

    a recursive function implemented to find out factorial

---
+++ Recurtion/src/lab/exercise/RecursiveFunction.java
@@ -0,0 +1,16 @@
+package lab.exercise;
+
+public class RecursiveFunction {
+	public static int factorial(int n) {
+		if(n==1)return 1;
+		else {
+			return n* factorial(n-1);
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(factorial(4));
+
+	}
+
+}
